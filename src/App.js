0781786e677912f0f15a7e51255a1f5f62e93dd3
Scott Sandman <eightmenout19@gmail.com>
2020-04-29 11:24:30 -0400

    created counter using useReducer hook, modified reducer to use switch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from "react";
+import "./App.css";
+
+const initialState = {
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT_COUNTER":
+      return { ...state, counter: state.counter + 1 };
+    case "DECREMENT_COUNTER":
+      return { ...state, counter: state.counter - 1 };
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state.counter);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{state.counter}</div>
+        <button onClick={() => dispatch({ type: "INCREMENT_COUNTER" })}>
+          increment
+        </button>
+        <button onClick={() => dispatch({ type: "DECREMENT_COUNTER" })}>
+          decrement
+        </button>
       </header>
     </div>
   );
