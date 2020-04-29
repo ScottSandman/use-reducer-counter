2039ac4d747cf7a75d542dc003ace9d1c2eee3ad
Scott Sandman <eightmenout19@gmail.com>
2020-04-29 14:17:22 -0400

    moved action string types, action creators and reducers to separate files

--- src/App.js
+++ src/App.js
@@ -1,61 +1,20 @@
 import React, { useReducer, useState } from "react";
 import "./App.css";
-
-const INCREMENT_COUNTER = "INCREMENT_COUNTER";
-const DECREMENT_COUNTER = "DECREMENT_COUNTER";
-const UPDATE_N = "UPDATE_N";
-const INCREMENT_BY_N = "INCREMENT_BY_N";
-const DECREMENT_BY_N = "DECREMENT_BY_N";
-const ALLOW_INCREMENT_BY_N = "ALLOW_INCREMENT_BY_N";
+import {
+  incrementCounter,
+  decrementCounter,
+  updateN,
+  incrementByN,
+  decrementByN,
+  allowIncrementByN,
+} from "./actions";
+import { reducer } from "./reducers";
 
 const initialState = {
   counter: 0,
   n: 0,
   allowed: true,
 };
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case INCREMENT_COUNTER:
-      return { ...state, counter: state.counter + 1 };
-    case DECREMENT_COUNTER:
-      return { ...state, counter: state.counter - 1 };
-    case UPDATE_N:
-      return { ...state, n: action.n };
-    case INCREMENT_BY_N:
-      return { ...state, counter: state.counter + state.n };
-    case DECREMENT_BY_N:
-      return { ...state, counter: state.counter - state.n };
-    case ALLOW_INCREMENT_BY_N:
-      return { ...state, allowed: !state.allowed };
-    default:
-      return state;
-  }
-};
-
-function incrementCounter() {
-  return { type: INCREMENT_COUNTER };
-}
-
-function decrementCounter() {
-  return { type: DECREMENT_COUNTER };
-}
-
-function updateN(n) {
-  return { type: UPDATE_N, n: n };
-}
-
-function incrementByN() {
-  return { type: INCREMENT_BY_N };
-}
-
-function decrementByN() {
-  return { type: DECREMENT_BY_N };
-}
-
-function allowIncrementByN() {
-  return { type: ALLOW_INCREMENT_BY_N };
-}
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
